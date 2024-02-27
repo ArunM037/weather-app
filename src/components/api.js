ac49e00ea1741e73bcf7a0c0372eb17139736447
ArunM037 <156396808+ArunM037@users.noreply.github.com>
2024-02-27 13:44:44 +0530

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'f986e7c8a0mshadc2b4526605121p1e5c6ejsn2bbf85a28d3f',
+        'X-RapidAPI-Key': 'Geo_API_KEY',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "d04e5b43cd94d709d07a474fd86b74f7";  
+export const WEATHER_API_KEY = "WEATHER_API-KEY";  
