b0177516fd73f9f2f9ae80bc20b4fc593f0ae3f2
Arun M <arunm2020@psnacet.edu.in>
2024-02-27 13:40:10 +0530

    https://github.com/ArunM037/Weather-react-_app.git

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f986e7c8a0mshadc2b4526605121p1e5c6ejsn2bbf85a28d3f',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d04e5b43cd94d709d07a474fd86b74f7";  
